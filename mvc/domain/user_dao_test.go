d566ecc1c059a9cc35b5a94134016d4ac7639e8c
Pappu Kumar <36985548+davetweetlive@users.noreply.github.com>
2020-09-03 00:34:34 +0530

    Testing merge to feature branch (#3)
    * adding basic MVC structure (#1)
    
    * Testing

---
+++ mvc/domain/user_dao_test.go
@@ -0,0 +1,33 @@
+package domain
+
+import (
+	"net/http"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestGetUserNoUserFound(t *testing.T) {
+	// Initialization
+
+	// Execution
+	user, err := GetUser(0)
+
+	// Validation
+	assert.Nil(t, user, "We were not expecting a user with id 0")
+	assert.NotNil(t, err)
+	assert.EqualValues(t, err.StatusCode, http.StatusNotFound)
+	assert.EqualValues(t, err.Code, "not_found")
+	assert.EqualValues(t, err.Message, "user 0 does not exists")
+}
+
+func TestGetUserNoError(t *testing.T) {
+	user, err := GetUser(123)
+	assert.Nil(t, err)
+	assert.NotNil(t, user)
+	assert.EqualValues(t, 1, user.ID)
+	assert.EqualValues(t, "Dave", user.FirstName)
+	assert.EqualValues(t, "Augustus", user.LastName)
+	assert.EqualValues(t, "email@email.com", user.Email)
+	//
+}
